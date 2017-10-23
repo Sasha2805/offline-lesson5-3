bdef8e0c37a064fabb104e6a64ac23c19d717498
Sasha Kaminska <sasha249133@ukr.net>
2017-10-23 21:46:29 +0300

    Проверка работы классов

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import company.Company;
+import employees.FixedWageEmployee;
+import employees.Freelancer;
+import employees.HourlyEmployee;
 
 public class Main {
+    private static final String PATH = "src/data_employees.txt";
+
+    public static void main(String[] args) {
+        Company company = new Company();
+        company.addEmployee(new HourlyEmployee("John", "Smith", 11));
+        company.addEmployee(new FixedWageEmployee("Bob", "Smith", 1500));
+        company.addEmployee(new Freelancer("Rachel", "Oliver", 5, 10));
+        company.saveEmployeesInFile(PATH);
+
+        System.out.println(company.monthlyPaymentWages());
+        company.show();
+
+        company.sortWagesAscending();
+        company.show();
+
+        company.sortWagesDescending();
+        company.show();
+    }
 }
